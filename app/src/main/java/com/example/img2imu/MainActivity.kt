541a662a4c1d85edd4e458a713001aa684b1de6e
윤형준 <dbslzhem16@gmail.com>
2023-02-07 16:18:11 +0900

    made the app to average the last 10 trials' result

--- app/src/main/java/com/example/img2imu/MainActivity.kt
+++ app/src/main/java/com/example/img2imu/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
@@ -23,28 +24,42 @@ class MainActivity : AppCompatActivity() {
         val inputDescription = findViewById<TextView>(R.id.inputDescription)
         inputDescription.text = "input: $signalPath\nmodel: $modelPath"
 
-        val signalProcessor = SignalProcessor(applicationContext)
-        var (spectrogramBitmap, SPtime) = signalProcessor.createSpectrogram(signalPath)
-        spectrogramBitmap = Bitmap.createScaledBitmap(spectrogramBitmap,
-            128, 96, false)
+        var trials = 0
 
-        val spectrogramImage = findViewById<ImageView>(R.id.image1)
-        spectrogramImage.setImageBitmap(spectrogramBitmap)
+        val SPTimeQueue = ArrayDeque<Float>(10)
+        val IFTimeQueue = ArrayDeque<Float>(10)
 
-        val inferenceManager = InferenceManager(applicationContext)
-        val (outIdx, IFtime) = inferenceManager.performPytorchInference(modelPath,
-            spectrogramBitmap)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            trials += 1
 
-        if (outIdx == 1.0f) {
-            val inferenceResult = findViewById<TextView>(R.id.inferenceResult)
-            inferenceResult.text = "Result: Jogging"
-        } else {
-            val inferenceResult = findViewById<TextView>(R.id.inferenceResult)
-            inferenceResult.text = "Result: $outIdx"
+            val signalProcessor = SignalProcessor(applicationContext)
+            var (spectrogramBitmap, SPtime) = signalProcessor.createSpectrogram(signalPath)
+            spectrogramBitmap = Bitmap.createScaledBitmap(spectrogramBitmap,
+                128, 96, false)
+
+            val spectrogramImage = findViewById<ImageView>(R.id.image1)
+            spectrogramImage.setImageBitmap(spectrogramBitmap)
+
+            val inferenceManager = InferenceManager(applicationContext)
+            val (outIdx, IFtime) = inferenceManager.performPytorchInference(modelPath,
+                spectrogramBitmap)
+
+            if (outIdx == 1.0f) {
+                val inferenceResult = findViewById<TextView>(R.id.inferenceResult)
+                inferenceResult.text = "Result: Jogging"
+            } else {
+                val inferenceResult = findViewById<TextView>(R.id.inferenceResult)
+                inferenceResult.text = "Result: $outIdx"
+            }
+
+            SPTimeQueue.offer(SPtime.toFloat())
+            IFTimeQueue.offer(IFtime.toFloat())
+
+            val avgSPtime = SPTimeQueue.average()
+            val avgIFtime = IFTimeQueue.average()
+            val outtext = findViewById<TextView>(R.id.outtext)
+            outtext.text = "Trials: $trials (average calculated for last 10 trials)\nTransformation time: $avgSPtime ms\nInference time: $avgIFtime ms"
         }
-
-        val outtext = findViewById<TextView>(R.id.outtext)
-        outtext.text = "Transformation time: $SPtime ms\nInference time: $IFtime ms"
     }
 
 }
